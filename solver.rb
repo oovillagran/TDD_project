6448a4c3fa2e7191e3825a663e79f245edaa25a2
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-07-06 12:17:05 -0300

    solve rubocop linters problems

--- solver.rb
+++ solver.rb
@@ -1,9 +1,9 @@
 class Solver
-  def factorial(n)
-    raise ArgumentError, 'Argument must be a non-negative integer' if n.negative?
+  def factorial(numb)
+    raise ArgumentError, 'Argument must be a non-negative integer' if numb.negative?
 
     result = 1
-    (1..n).each { |num| result *= num }
+    (1..numb).each { |num| result *= num }
     result
   end
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Solver do
   describe '#factorial' do
     it 'returns the factorial of a number' do
       solver = Solver.new
-        expect(solver.factorial(0)).to eq(1)
-        expect(solver.factorial(5)).to eq(120)
-        expect(solver.factorial(6)).to eq(720)
+      expect(solver.factorial(0)).to eq(1)
+      expect(solver.factorial(5)).to eq(120)
+      expect(solver.factorial(6)).to eq(720)
     end
 
     it 'raises an exception for negative integers' do
@@ -16,10 +16,10 @@ RSpec.describe Solver do
   end
 
   describe '#reverse' do
-  it 'returns the reversed string' do
-    solver = Solver.new
-    expect(solver.reverse('hello')).to eq('olleh')
-    expect(solver.reverse('world')).to eq('dlrow')
+    it 'returns the reversed string' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('world')).to eq('dlrow')
+    end
   end
-end
 end
