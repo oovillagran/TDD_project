b04b8389b3cbe802f2019bc63d40f28b1d99b8fb
oovillagran <oscar.villagran@hotmail.com>
2023-07-06 11:22:39 -0500

    Add a condition if a number is divisible by 5 and by 3 in the fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -12,6 +12,6 @@ class Solver
   end
 
   def fizzbuzz(num)
-    num % 3 == 0 ? "fizz" : num % 5 == 0 ? 'buzz' : num
+    num % 3 ==0 && num % 5 == 0 ? 'fizzbuzz' : num % 3 == 0 ? "fizz" : num % 5 == 0 ? 'buzz' : num
   end
 end
