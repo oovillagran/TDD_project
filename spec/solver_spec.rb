de223a7ecaec9efcdea8002bd2bed2e71f590837
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-07-06 12:00:04 -0300

    added test for solver

---
+++ spec/solver_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../solver'
+
+RSpec.describe Solver do
+  describe '#factorial' do
+    it 'returns the factorial of a number' do
+      solver = Solver.new
+        expect(solver.factorial(0)).to eq(1)
+        expect(solver.factorial(5)).to eq(120)
+        expect(solver.factorial(6)).to eq(720)
+    end
+
+    it 'raises an exception for negative integers' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
+end
